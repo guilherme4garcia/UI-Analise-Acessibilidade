56acf0e7a96229f93c29153208c036026d5558db
Guilherme Garcia <guilherme4garcia@gmail.com>
2022-11-27 13:08:16 -0300

    Update Singleton.java

--- ESIII/Singleton/src/company/Singleton.java
+++ ESIII/Singleton/src/company/Singleton.java
@@ -7,11 +7,14 @@ public class Singleton {
     private static volatile Singleton instance;
 
     private Singleton(String data) {
+        //Singleton simulando a conexão de apenas um database
         this.data = data;
     }
 
     public static Singleton getInstance(String data) {
         Singleton result = instance;
+        
+        // if para garantir que exista apenas uma instância
         if (result == null) {
             synchronized (Singleton.class) {
                 result = instance;
